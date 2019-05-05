b5d9c3b6edea952dcb2740cdba2d58f974f6b7b0
SaGoR <sagordj07@gmail.com>
2019-05-05 13:32:22 +0600

    add file 4th group chat Activity

--- app/src/main/java/com/messengers/swipechat/swipechats/GroupMassageActivity.java
+++ app/src/main/java/com/messengers/swipechat/swipechats/GroupMassageActivity.java
@@ -1,6 +1,7 @@
 package com.messengers.swipechat.swipechats;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +24,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class GroupMassageActivity extends AppCompatActivity {
     private Toolbar mtoolbar;
@@ -42,12 +45,10 @@ public class GroupMassageActivity extends AppCompatActivity {
         currentgroupName=getIntent().getExtras().get("Group name").toString();
         Toast.makeText(GroupMassageActivity.this,currentgroupName,Toast.LENGTH_LONG).show();
 
-
         mAuth=FirebaseAuth.getInstance();
         currentUserId=mAuth.getCurrentUser().getUid();
 
-        userRef=FirebaseDatabase.getInstance().getReference().child("Users")
-                .child(currentgroupName);
+        userRef=FirebaseDatabase.getInstance().getReference().child("Users");
         GroupNameRef=FirebaseDatabase.getInstance().getReference().child("Groups").child(currentgroupName);
 
 
@@ -66,9 +67,56 @@ public class GroupMassageActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GroupNameRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
+            {
+
+                if(dataSnapshot.exists())
+                {
+                    DisplayMassage(dataSnapshot);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s)
+            {
+                if(dataSnapshot.exists())
+                {
+                    DisplayMassage(dataSnapshot);
+                }
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+
     private void saveMassage() {
         String message=usermassgageinput.getText().toString();
         String massageKey=GroupNameRef.push().getKey();
+
 
         if(TextUtils.isEmpty(message))
         {
@@ -99,12 +147,29 @@ public class GroupMassageActivity extends AppCompatActivity {
 
             groupMassageKeyref.updateChildren(massagInfo);
 
+        }
+    }
 
+    private void DisplayMassage(DataSnapshot dataSnapshot)
+    {
+        Iterator iterator=dataSnapshot.getChildren().iterator();
+        while (iterator.hasNext())
+        {
+            String massgdate=(String)((DataSnapshot)iterator.next()).getValue();
+            String massageTime=(String)((DataSnapshot)iterator.next()).getValue();
+            String maassage=(String)((DataSnapshot)iterator.next()).getValue();
+            String massagename=(String)((DataSnapshot)iterator.next()).getValue();
 
+            massagetextdisplay.append(massagename+"\n");
+            massagetextdisplay.append(maassage+"\n");
+            massagetextdisplay.append(massageTime+"\n");
+            massagetextdisplay.append(massgdate+"\n");
 
 
 
         }
+
+
     }
 
     private void GetUserDetails() {
